906833230cbe6a740488a9ecbe197c0c4a3cc5f7
Dmitrii Anufriev <anufrievdr@gmail.com>
2020-04-06 16:28:31 +0300

    Pylint and pydoctyle fix

--- Chapter 2/recursion_palindrome.py
+++ Chapter 2/recursion_palindrome.py
@@ -6,14 +6,14 @@ WORDS_LIST = load_dictionary.load("2of4brif.txt")
 
 
 def searcher(word):
-    if len(word) == 0 or len(word) == 1:
-        return True
-    else:
-        if word[0] == word[-1]:
-            return searcher(word[1:-1])
+    """Recusrsion search of palindromes."""
+    if len(word) <= 1:
+        return word
+    if word[0] == word[-1]:
+        return searcher(word[1:-1])
 
 
 if __name__ == "__main__":
-    for word in WORDS_LIST:
-        if searcher(word):
-            print(word)
+    for i in WORDS_LIST:
+        if searcher(i):
+            print(i)
